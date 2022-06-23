7cc8849b263625019f84c73d5d1c0600eb6d79ee
denisdiaconu <denisdiaconu117@yahoo.com>
2022-06-23 13:11:14 +0200

    Add sidebar style

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,7 +1,8 @@
-import { Avatar } from '@material-ui/core';
+import { Avatar, IconButton } from '@material-ui/core';
 import ChatIcon from '@material-ui/icons/Chat';
 import DonutLargeIcon from '@material-ui/icons/DonutLarge';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
+import SearchIcon from '@material-ui/icons/Search';
 import React from 'react';
 import './Sidebar.css';
 
@@ -11,13 +12,23 @@ function Sidebar() {
         <div className="sidebar_top">
             <Avatar />
             <div className="sidebar_right">
-                <DonutLargeIcon />
-                <ChatIcon />
-                <MoreVertIcon />
+                <IconButton>
+                    <DonutLargeIcon />
+                </IconButton>
+                <IconButton>
+                    <ChatIcon />
+                </IconButton>
+                <IconButton>
+                    <MoreVertIcon />  
+                </IconButton>
             </div>
         </div>
         <div className="sidebar_search">
+            <div className="search_cont">
+                <SearchIcon />  
+                <input type="text" placeholder='Search or start a new chat' />
 
+            </div>
         </div>
         <div className="sidebar_chats">
 
