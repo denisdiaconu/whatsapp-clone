b81a3d336530c79722d31dd084b7f150a445a844
denisdiaconu <denisdiaconu117@yahoo.com>
2022-06-23 12:31:46 +0200

    Add material ui icons

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,3 +1,7 @@
+import { Avatar } from '@material-ui/core';
+import ChatIcon from '@material-ui/icons/Chat';
+import DonutLargeIcon from '@material-ui/icons/DonutLarge';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
 import React from 'react';
 import './Sidebar.css';
 
@@ -5,13 +9,18 @@ function Sidebar() {
   return (
     <div className='sidebar'>
         <div className="sidebar_top">
-
+            <Avatar />
+            <div className="sidebar_right">
+                <DonutLargeIcon />
+                <ChatIcon />
+                <MoreVertIcon />
+            </div>
         </div>
         <div className="sidebar_search">
 
         </div>
         <div className="sidebar_chats">
-            
+
         </div>
     </div>
   )
