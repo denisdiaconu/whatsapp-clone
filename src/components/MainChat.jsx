dddce896f8cf164d11d5f4ceb4753a68a3ed9535
denisdiaconu <denisdiaconu117@yahoo.com>
2022-06-24 11:31:55 +0200

    Add style for main chat

--- src/components/MainChat.jsx
+++ src/components/MainChat.jsx
@@ -1,8 +1,51 @@
 import React from 'react'
+import './MainChat.css'
+import { Avatar, IconButton } from '@material-ui/core';
+import SearchIcon from '@material-ui/icons/Search';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import MicIcon from '@material-ui/icons/Mic';
+import InsertEmoticonIcon from '@material-ui/icons/InsertEmoticon';
 
 function MainChat() {
+
+    const sendMessage = (event) => {
+
+    }
+
+
   return (
-    <div>MainChat</div>
+    <div className="main_chat">
+        <div className='chat_top'>
+            <Avatar src={`https://avatars.dicebear.com/api/gridy/${Math.random()}.svg`} />
+            <div className='chat_top_details'>
+                <h2>Room</h2>
+                <p>last on</p>
+            </div>
+            <div className="chat_top_right">
+                <IconButton>
+                    <SearchIcon />
+                </IconButton>
+                <IconButton>
+                    <MoreVertIcon />  
+                </IconButton>
+            </div>
+        </div>
+        <div className="chat_body">
+            <p className={`chat_message ${true && "chat_rec"}`}>
+                <span className="chat_name">Des</span>
+                Hello my friend
+                <span className="chat_time">7:34pm</span>
+            </p>
+        </div>
+        <div className='chat_bottom'>
+            <InsertEmoticonIcon />
+            <form>
+                <input type="text" placeholder="Type a message" />
+                <button onclick={sendMessage} type='submit'>Send</button>
+            </form>
+            <MicIcon />
+        </div>
+    </div>
   )
 }
 
