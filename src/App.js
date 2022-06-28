3a5454f08f6c27133bb9cadb461dbbe28a3d3640
denisdiaconu <denisdiaconu117@yahoo.com>
2022-06-28 11:24:30 +0200

    Add homeChat component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import './App.css';
 import Sidebar from './components/Sidebar';
 import MainChat from './components/MainChat';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import HomeChat from './components/HomeChat';
+
 
 function App() {
   return (
     <div className="App">
       <div className="app_body">
-        <Sidebar />
-        <MainChat />
+        <Router>
+          <Sidebar />
+          <Routes>
+            <Route path='/rooms/:roomId' element={ <MainChat /> } />
+            <Route path='/' element= {<HomeChat />} />
+          </Routes>
+        </Router>
       </div>
     </div>
   );
